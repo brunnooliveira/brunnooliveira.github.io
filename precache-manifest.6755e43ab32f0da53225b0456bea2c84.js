9badca54e6c78d120ca52b80d8f470a157244dae
Brunno <brunnogoncalves@gmail.com>
2020-07-05 17:11:07 -0300

    Migrando pra React

---
+++ precache-manifest.6755e43ab32f0da53225b0456bea2c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b18ad027c7508ddabe41d3b2918a68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4fb7603154c520166cf",
+    "url": "/static/css/main.104f07ec.chunk.css"
+  },
+  {
+    "revision": "398a8d90585609d3fa0e",
+    "url": "/static/js/2.1eaa9ea0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1eaa9ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fb7603154c520166cf",
+    "url": "/static/js/main.8da738f9.chunk.js"
+  },
+  {
+    "revision": "a5fb0d9fcfbee188e342",
+    "url": "/static/js/runtime-main.a12ac742.js"
+  }
+]);
