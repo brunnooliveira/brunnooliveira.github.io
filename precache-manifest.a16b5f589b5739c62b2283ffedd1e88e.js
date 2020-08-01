7b15c7afc6d4fab11f21a04d94b1d3e89655e71e
Brunno <brunnogoncalves@gmail.com>
2020-08-01 16:03:26 -0300

    Inclusão de calculo RM scalp swing

---
+++ precache-manifest.a16b5f589b5739c62b2283ffedd1e88e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3518b2fafec3b54f405ef5ce3a9da476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1d0942357317116d47a",
+    "url": "/static/css/main.104f07ec.chunk.css"
+  },
+  {
+    "revision": "a1de9740a3bb07ef99ce",
+    "url": "/static/js/2.1d2d2b1a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1d2d2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21852044326653676bb4",
+    "url": "/static/js/3.fc814a9a.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.fc814a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d0942357317116d47a",
+    "url": "/static/js/main.251493f5.chunk.js"
+  },
+  {
+    "revision": "c4b8c43f6ff59f5f12dc",
+    "url": "/static/js/runtime-main.983f58a3.js"
+  },
+  {
+    "revision": "fec033b50f46a4b11bf244802ba20191",
+    "url": "/static/media/naosejaotario.fec033b5.mp3"
+  }
+]);
